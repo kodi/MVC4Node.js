c19cf6cc4dacd851e777a4a14ecfb4a9aca97738
kodi <dragan.bajcic@gmail.com>
2010-05-27 20:55:50 +0200

    cleanup

--- application/controllers/controller_home.js
+++ application/controllers/controller_home.js
@@ -2,13 +2,10 @@ var sys = require('sys');
 
 var home = function(){
 
-    
     this.index = function(){
         this.response.writeHead(200, {'Content-Type': 'text/plain'});
         this.response.end('Hello World\n');
     }    
-    
-    
 }
 
 exports.controller_home =  home;
--- server.js
+++ server.js
@@ -2,22 +2,20 @@ require.paths.unshift(__dirname);
 
 var sys = require('sys'),
   http = require('http'),
-  engine = require('engine'),
   requestModule = require('lib/request'),
   frontController = require('lib/FrontController');
   
 
 http.createServer(function (request, response) {
-  var r = new requestModule.Request();
-  var requestObject = r.parseRequest(request);
-
-
+  
+  // parse request
+  var requestObject = requestModule.Request.parseRequest(request);
+  // pass the response
   frontController.FrontController.setResponse(response);
+  // execute
   frontController.FrontController.execute(requestObject);
   
-  
-  
+
 }).listen(8000);
 
-sys.puts('require paths: '+require.paths);
 sys.puts('Server running at http://127.0.0.1:8000/');
