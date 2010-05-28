df134cf1ef91f796c69f3d42bbf9ccf3a8a849f1
kodi <dragan.bajcic@gmail.com>
2010-05-28 03:41:31 +0200

    code cleanup

--- application/controllers/controller_home.js
+++ application/controllers/controller_home.js
@@ -1,11 +1,11 @@
 var sys = require('sys');
 
-var home = function(){
+var home = function() {
 
-    this.index = function(){
-		this.template.assign('firstName','Kodi');
-		this.template.show('index.html');
-    }    
+    this.index = function() {
+        this.template.assign('firstName', 'Kodi');
+        this.template.show('index.html');
+    }
 }
 
-exports.controller_home =  home;
+exports.controller_home = home;
